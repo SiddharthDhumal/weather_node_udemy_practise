62069f469710452df9c46dd136ad31c1b3ac2c78
unknown <siddharthdhumal210@gmail.com>
2021-10-02 18:46:29 +0530

    remove unncessary comments

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const notes = require('./notes.js')
 // Customize yargs version
 yargs.version('1.1.0')
 
-// Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
